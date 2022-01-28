7a0091731e6a1609c88dc6d943586fa2e5da4605
raulvictorvieira <raulvictorsv@gmail.com>
2022-01-28 15:01:10 -0300

    server

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const server = express();
+const routes = require('./routes');
+
+server.use(express.json());
+server.use(routes);
+
+const PORT = 3333;
+server.listen(PORT, () => console.log('Executando'));
